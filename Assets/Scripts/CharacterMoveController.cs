f6b7c1c760b2c4bf0d16ac9b397d000fd37fcbb8
Alexey <leshaego14@gmail.com>
2019-10-07 01:15:37 +0300

    Start a new game
    I have found an asset pack for 2d game.
    In this commit there were added a background, character, his animations and move controller.

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+
+
+    public Transform groundChecker;
+    public float groundRadius = 0.2f;
+    public float spaceForce = 1f;
+    public LayerMask groundLayer;
+    private bool isGrounded = true;
+
+
+    private Animator animator;
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0, spaceForce));
+        }
+    }
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundChecker.position, groundRadius, groundLayer);
+
+        animator.SetBool("Ground", isGrounded);
+        animator.SetFloat("VerticalSpeed", rigidbody2D.velocity.y);
+
+        if (!isGrounded)
+            return;
+
+        float move = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Speed", Mathf.Abs(move));
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+    }
+}
