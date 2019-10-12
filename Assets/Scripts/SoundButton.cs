4f19e6f89aec05945a335ad02963b258cceeb007
Alexey <leshaego14@gmail.com>
2019-10-13 01:07:24 +0300

    Added sound mute

---
+++ Assets/Scripts/SoundButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour
+{
+    public Sprite activeSound, disableSound;
+    public void DoOnClick()
+    {
+        AudioListener mainCameraAudioListener = Camera.main.GetComponent<AudioListener>();
+
+        if (mainCameraAudioListener.enabled)
+        {
+            mainCameraAudioListener.enabled = false;
+            GetComponent<Image>().sprite = disableSound;
+        } else
+        {
+            mainCameraAudioListener.enabled = true;
+            GetComponent<Image>().sprite = activeSound;
+        }
+    }
+}
