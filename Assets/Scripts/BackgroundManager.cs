226f0ac6f3b63bf04e64ef4ba95c391a56032569
Alexey <leshaego14@gmail.com>
2019-10-12 13:48:42 +0300

    Fixed bag with water
    Also refactored the BackgroundManager script.

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -8,35 +8,38 @@ public class BackgroundManager : MonoBehaviour
     public Vector3 mainCameraStartPoint = new Vector3(-10, 0, -10);
     public GameObject backgroundPrefab;
     public Transform[] backgrounds;
-    public float delayBetweenDestroyingOldBacks = 2f;
+    public float delayBetweenDestroyingOldBacks = 1f;
     private Transform[] newBackgrounds;
     public Vector3 background3StartPoint = new Vector3(-19.2f, 0, 0);
     public float maxRangeBetweenMainCameraAndLastBackground = 35.2f;
     public float rangeBetweenNextBackgrounds = 19.2f;
 
-    private bool wasSteped = false;
-
-    // Start is called before the first frame update
+    private bool steped = false;
     void Start()
     {
         SetBackgroundsToStartPositions(backgrounds);
+        newBackgrounds = CreateNewBackgrounds();
     }
     private void SetBackgroundsToStartPositions(Transform[] backgrounds)
     {
-        backgrounds[0].position = background3StartPoint;
-
-        Vector3 point = new Vector3(background3StartPoint.x + rangeBetweenNextBackgrounds,
+        Vector3 point = new Vector3(background3StartPoint.x,
                                     background3StartPoint.y,
                                     background3StartPoint.z);
+        backgrounds[0].position = point;
+
+
+        point = new Vector3(background3StartPoint.x + rangeBetweenNextBackgrounds,
+                            background3StartPoint.y,
+                            background3StartPoint.z);
         backgrounds[1].position = point;
+
+
         point = new Vector3(point.x + rangeBetweenNextBackgrounds,
                             point.y,
                             point.z);
         backgrounds[2].position = point;
-        wasSteped = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float mainCameraX = mainCamera.position.x;
@@ -44,19 +47,14 @@ public class BackgroundManager : MonoBehaviour
 
         if (mainCameraX - lastBackgroundX > maxRangeBetweenMainCameraAndLastBackground)
         {
-            if (!wasSteped)
-            {
-
-                newBackgrounds = CreateNewBackgrounds();
-                SetBackgroundsToStartPositions(newBackgrounds);
-            }
             StepBackgrounds();
+            steped = true;
         }
 
-        if (mainCameraX == mainCameraStartPoint.x && wasSteped)
+        if (mainCameraX == mainCameraStartPoint.x && steped)
         {
-            SetBackgroundsToStartPositions(newBackgrounds);
-            DeleteOldBackgrounds(newBackgrounds);
+            DeleteOldBackgrounds();
+            steped = false;
         }
     }
     private void StepBackgrounds()
@@ -69,8 +67,6 @@ public class BackgroundManager : MonoBehaviour
         backgrounds[0] = backgrounds[1];
         backgrounds[1] = backgrounds[2];
         backgrounds[2] = temp;
-
-        wasSteped = true;
     }
     private Transform[] CreateNewBackgrounds()
     {
@@ -79,16 +75,39 @@ public class BackgroundManager : MonoBehaviour
         array[1] = Instantiate(backgroundPrefab).transform;
         array[2] = Instantiate(backgroundPrefab).transform;
 
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i].transform.SetParent(transform);
+        }
+        SetBackgroundsToStartPositions(array);
+        HideBackgrounds(array);
+
         return array;
     }
-    private void DeleteOldBackgrounds(Transform[] newBackgrounds)
+    private void HideBackgrounds(Transform[] backgrounds)
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            Vector3 newPosition = new Vector3(backgrounds[i].transform.position.x,
+                                              backgrounds[i].transform.position.y,
+                                              backgrounds[i].transform.position.z + 2);
+            backgrounds[i].transform.position = newPosition;
+        }
+    }
+    private void DeleteOldBackgrounds()
     {
         Transform[] forDestroying = { backgrounds[0], backgrounds[1], backgrounds[2] };
         Destroy(forDestroying[0].gameObject, delayBetweenDestroyingOldBacks);
         Destroy(forDestroying[1].gameObject, delayBetweenDestroyingOldBacks);
         Destroy(forDestroying[2].gameObject, delayBetweenDestroyingOldBacks);
-        backgrounds[0] = newBackgrounds[0];
-        backgrounds[1] = newBackgrounds[1];
-        backgrounds[2] = newBackgrounds[2];
+        backgrounds = newBackgrounds;
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            Vector3 newPosition = new Vector3(backgrounds[i].transform.position.x,
+                                              backgrounds[i].transform.position.y,
+                                              backgrounds[i].transform.position.z - 2);
+            backgrounds[i].transform.position = newPosition;
+        }
     }
 }
